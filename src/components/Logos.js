7b34ad7672c5af3be447dc1e10f9590313317b1c
Nha Thi Vuong <nhathivuong@gmail.com>
2025-03-14 14:41:00 -0400

    portfolio

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -4,17 +4,17 @@
 const Logos = () => {
     return (
         <div className="flex justify-between items-center flex-row h-20 border-t-2">
-            <img className="max-h-15 p-1" src="/assets/html.png" alt="html logo"/>
-            <img className="max-h-15 p-1" src="/assets/css.png" alt="css logo"/>
-            <img className="max-h-15 p-1" src="/assets/javascript.png" alt="javascript logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/html.png" alt="html logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/css.png" alt="css logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/javascript.png" alt="javascript logo"/>
             <div className="flex justify-between flex-row h-15 p-1">
-                <img src="/assets/github-mark.png" alt="github mark logo"/>
-                <img src="/assets/github-text.png" alt="github text logo"/>
+                <img src="/assets/logos/github-mark.png" alt="github mark logo"/>
+                <img src="/assets/logos/github-text.png" alt="github text logo"/>
             </div>
-            <img className="max-h-15 p-1" src="/assets/react.png" alt="react logo"/>
-            <img className="max-h-15 p-1" src="/assets/mongodb.png" alt="mongodb logo"/>
-            <img className="max-h-15 p-1" src="/assets/styled-components.png" alt="styled components logo"/>
-            <img className="max-h-15 p-1" src="/assets/tailwindcss-logotype.svg" alt="tailwind logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/react.png" alt="react logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/mongodb.png" alt="mongodb logo"/>
+            <img className="max-h-15 p-1" src="/assets/logos/styled-components.png" alt="styled components logo"/>
+            <img className="max-h-10 p-1" src="/assets/logos/tailwindcss-logotype.svg" alt="tailwind logo"/>
         </div>
     )
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,38 @@
+import { Features } from "tailwindcss"
+
 const Portfolio = () => {
-    return <p>portfolio</p>
+    return (<>
+        <h2>portfolio</h2>
+        <div>
+            <h3>Nyan Cat</h3>
+            <img src="/assets/portfolio/NyanCat.gif" alt="a space invader inspired game"/>
+            <h4>features</h4>
+            <ul>
+                <li><p>Collision detection</p></li>
+                <li><p>WASD and Arrow mouvement</p></li>
+                <li><p>Bonus granting immunity for the next collision</p></li>
+                <li><p>Bonus giving more point</p></li>
+                <li><p>Track a history of the best score using JSON</p></li>
+                <li><p>Increased difficulty with progression</p></li>
+                <li><p>Start and Restart button</p></li>
+            </ul>
+        </div>
+        <div>
+            <h3>tic-tac-toe</h3>
+            <h4>features</h4>
+            <ul>
+                <li><p>Winning condition detection</p></li>
+                <li><p>History of the winner</p></li>
+                <li>
+                    <p>Bot Ai</p>
+                    <ul>
+                        <li><p>plays a winning move</p></li>
+                        <li><p>plays a blocking move</p></li>
+                        <li><p>Plays in the middle square</p></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </>)
 }
 export default Portfolio
